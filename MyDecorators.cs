85ea098cb47c0030880f537df7e12a8d442faa66
Tomas Campos <p4rfait@localhost.localdomain>
2023-02-08 10:13:10 -0600

    First commit

---
+++ MyDecorators.cs
@@ -0,0 +1,80 @@
+namespace MyDecorators
+{
+    public class ClaseDecorators
+    {
+        public void Displaylogo(bool animation, int vel = 75)
+        {                
+            
+            string[] logo = {
+            "███████████████████████████████████████████████████████████████████████████████",
+            "",
+            "███████╗███████╗ ██████╗██╗   ██╗██████╗ ███████╗██╗  ██╗███████╗██╗   ██╗",
+            "██╔════╝██╔════╝██╔════╝██║   ██║██╔══██╗██╔════╝██║ ██╔╝██╔════╝╚██╗ ██╔╝",
+            "███████╗█████╗  ██║     ██║   ██║██████╔╝█████╗  █████╔╝ █████╗   ╚████╔╝ ",
+            "╚════██║██╔══╝  ██║     ██║   ██║██╔══██╗██╔══╝  ██╔═██╗ ██╔══╝    ╚██╔╝  ",
+            "███████║███████╗╚██████╗╚██████╔╝██║  ██║███████╗██║  ██╗███████╗   ██║   ",
+            "╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ",
+            "    ████████                    ",
+            "  ██        ██                  ",
+            "██            ██████████████████",
+            "██  ████                      ██",
+            "██  ████      ░░░░██░░██░░██░░██",
+            "██░░        ░░████  ██  ██  ██  ",
+            "  ██░░░░░░░░██                  ",
+            "    ████████                    ",
+            "",
+            "███████████████████████████████████████████████████████████████████████████████",
+            "",
+            "Script creado por Tomas Armando Campos"
+            };
+
+            string[] logo_alt = {
+            "   _______________  _____  ______ ________  __",
+            "  / __/ __/ ___/ / / / _ \\/ __/ //_/ __/\\ \\/ /",
+            " _\\ \\/ _// /__/ /_/ / , _/ _// ,< / _/   \\  / ",
+            "/___/___/\\___/\\____/_/|_/___/_/|_/___/   /_/  ",
+            "",
+            "Script creado por Tomas Armando Campos"
+            };
+
+            if (Console.WindowWidth >= 80 && Console.WindowHeight >= 26 ){
+                for (int i = 0; i < logo.Length; i++)
+                    {
+                    int x = (Console.WindowWidth - logo[i].Length) / 2;
+                    int y = Console.WindowHeight / Console.WindowWidth + i;
+
+                    Console.SetCursorPosition(x, y);
+                    if (animation == true){
+                        Thread.Sleep(vel);
+                    }
+                    Console.ForegroundColor=ConsoleColor.Yellow;
+                    Console.WriteLine(logo[i]);
+                    Console.ResetColor();
+                    }  
+            }
+            else if (Console.WindowWidth >= 45 && Console.WindowHeight >= 15 )
+            {
+                for (int i = 0; i < logo_alt.Length; i++)
+                    {
+                    int x = (Console.WindowWidth - logo_alt[i].Length) / 2;
+                    int y = Console.WindowHeight / Console.WindowWidth + i;
+
+                    Console.SetCursorPosition(x, y);
+                    if (animation == true){
+                        Thread.Sleep(vel);
+                    }
+                    Console.ForegroundColor=ConsoleColor.Yellow;
+                    Console.WriteLine(logo_alt[i]);
+                    Console.ResetColor();
+                    }  
+            }
+            
+        }
+        public void Separator(int linesQty)
+        {
+            Console.ForegroundColor=ConsoleColor.DarkCyan;
+            Console.WriteLine(new string('-', linesQty));
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Threading;
+//Importo mi archivo de
+using MyDecorators;
+
+namespace passgen;
+class Program
+{
+  static void Main(string[] args)
+  {
+    if (Console.WindowWidth < 45 || Console.WindowHeight < 15)
+    {
+      Console.ForegroundColor=ConsoleColor.DarkRed;
+      Console.Write("\x1b[1m");
+      Console.WriteLine("\nLa ventana es demasiado chica para ejecutar el programa\n");
+      Console.Write("\x1b[0m");
+      return;
+    };
+
+    //Llamando Decorators como Decorator
+    ClaseDecorators Decorador = new ClaseDecorators();
+
+    Console.Clear();
+    Console.CursorVisible = false;
+
+    Decorador.Displaylogo(true);
+    
+    Console.CursorVisible = true;
+
+
+    bool isValidInput = false;
+    while (!isValidInput)
+    {
+
+      checkwinsize();
+
+      Console.Clear();
+      Decorador.Displaylogo(false);
+      Decorador.Separator(5);
+      Console.Write("\x1b[1m");
+      Console.ForegroundColor=ConsoleColor.DarkGreen;
+      Console.WriteLine("Que accion desea realizar?:");
+      Console.Write("\x1b[0m");
+
+      
+      Console.ForegroundColor=ConsoleColor.White;
+      Console.WriteLine("(1) Menu de generacion de contraseña");
+      Console.WriteLine("(2) Acerca de & Créditos");
+      Console.ResetColor();
+
+      Console.ForegroundColor=ConsoleColor.Red;
+      Console.WriteLine("(3) Salir");
+      Console.ResetColor();
+
+      Decorador.Separator(5);
+      
+      Console.ForegroundColor=ConsoleColor.DarkGreen;
+      Console.Write(">");
+      Console.ResetColor();
+      Console.CursorVisible = true;
+      string? input = Console.ReadLine();
+      switch (input)
+      {
+        case "1":
+        checkwinsize();
+            isValidInput = true;
+            Console.WriteLine("Has seleccionado la opción 1");
+            Console.ReadKey();
+            break;
+        case "2":
+        checkwinsize();
+            isValidInput = true;
+            Console.WriteLine("Has seleccionado la opción 2");
+            Console.ReadKey();
+            break;
+        case "3":
+            Console.WriteLine("Saliendo del programa");
+            Console.ReadKey();
+            return;
+        default:
+            Console.CursorVisible = false;
+            Console.Write("\x1b[1m");
+            Console.ForegroundColor=ConsoleColor.DarkRed;
+            Console.WriteLine("\nOpcion no valida\n");
+            Console.ForegroundColor=ConsoleColor.White;
+            Console.WriteLine("Pulsa la tecla [Enter] para continuar...");
+            Console.Write("\x1b[0m");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) {}
+            break;
+      }
+    }
+
+    static void checkwinsize()
+    {
+      if (Console.WindowWidth < 45 || Console.WindowHeight < 15)
+      {
+        Console.Clear();
+        Console.ForegroundColor=ConsoleColor.DarkRed;
+        Console.Write("\x1b[1m");
+        Console.WriteLine("\nLa ventana es demasiado chica para continuar con la correcta ejecucion de el programa !!!\n");
+        Console.ForegroundColor=ConsoleColor.White;
+        Console.WriteLine("Pulsa la tecla [Enter] para salir ó ajuste el tamaño de la ventana y pulse la tecla [Enter] para continuar\n");
+        Console.Write("\x1b[0m");
+        while (Console.ReadKey().Key != ConsoleKey.Enter) {}
+        if (Console.WindowWidth < 45 || Console.WindowHeight < 15)
+        {
+          Environment.Exit(0);
+        }
+      };
+    }
+
+  }
+}
