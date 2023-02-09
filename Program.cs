839b8e129eab389b567958745c12524c638409ec
Tomas Campos <p4rfait@localhost.localdomain>
2023-02-09 13:02:18 -0600

    Se agrego el menu principal como funcion para mejorar el orden

--- Program.cs
+++ Program.cs
@@ -16,67 +16,72 @@ class Program
       Console.Write("\x1b[0m");
       return;
     };
-    ClaseDecorators Decorador = new ClaseDecorators();
-    Console.Clear();
-    Console.CursorVisible = false;
-    Decorador.Displaylogo(true);
-    Console.CursorVisible = true;
-    bool isValidInput = false;
-    while (!isValidInput)
+    MainMenu();
+    
+    static void MainMenu()
     {
-
-      checkwinsize();
-
+      ClaseDecorators Decorador = new ClaseDecorators();
       Console.Clear();
-      Decorador.Displaylogo(false);
-      Decorador.Separator(5);
-      Console.Write("\x1b[1m");
-      Console.ForegroundColor=ConsoleColor.DarkGreen;
-      Console.WriteLine("Que accion desea realizar?:");
-      Console.Write("\x1b[0m");
-      Console.ForegroundColor=ConsoleColor.White;
-      Console.WriteLine("(1) Menu de generacion de contraseña");
-      Console.WriteLine("(2) Acerca de & Créditos");
-      Console.ResetColor();
-      Console.ForegroundColor=ConsoleColor.Red;
-      Console.WriteLine("(3) Salir");
-      Console.ResetColor();
-      Decorador.Separator(5);
-      Console.ForegroundColor=ConsoleColor.DarkGreen;
-      Console.Write(">");
-      Console.ResetColor();
+      Console.CursorVisible = false;
+      Decorador.Displaylogo(true);
       Console.CursorVisible = true;
-      string? input = Console.ReadLine();
-      switch (input)
+      bool isValidInput = false;
+      while (!isValidInput)
       {
-        case "1":
         checkwinsize();
-            isValidInput = true;
-            Console.WriteLine("Has seleccionado la opción 1");
-            Console.ReadKey();
-            break;
-        case "2":
-        checkwinsize();
-            isValidInput = true;
-            Console.WriteLine("Has seleccionado la opción 2");
-            Console.ReadKey();
-            break;
-        case "3":
-            Console.WriteLine("Saliendo del programa");
-            Console.ReadKey();
-            return;
-        default:
-            Console.CursorVisible = false;
-            Console.Write("\x1b[1m");
-            Console.ForegroundColor=ConsoleColor.DarkRed;
-            Console.WriteLine("\nOpcion no valida\n");
-            Console.ForegroundColor=ConsoleColor.White;
-            Console.WriteLine("Pulsa la tecla [Enter] para continuar...");
-            Console.Write("\x1b[0m");
-            while (Console.ReadKey().Key != ConsoleKey.Enter) {}
-            break;
+        Console.Clear();
+        Decorador.Displaylogo(false);
+        Decorador.Separator(5);
+        Console.Write("\x1b[1m");
+        Console.ForegroundColor=ConsoleColor.DarkGreen;
+        Console.WriteLine("Que accion desea realizar?:");
+        Console.Write("\x1b[0m");
+        Console.ForegroundColor=ConsoleColor.White;
+        Decorador.Separator(5);
+        Console.WriteLine("(1) Menu de generacion de contraseña");
+        Console.WriteLine("(2) Acerca de & Créditos");
+        Console.ResetColor();
+        Console.ForegroundColor=ConsoleColor.Red;
+        Console.WriteLine("(3) Salir");
+        Console.ResetColor();
+        Decorador.Separator(5);
+        Console.ForegroundColor=ConsoleColor.DarkGreen;
+        Console.Write(">");
+        Console.ResetColor();
+        Console.CursorVisible = true;
+        string? input = Console.ReadLine();
+        switch (input)
+        {
+          case "1":
+          checkwinsize();
+              isValidInput = true;
+              Console.WriteLine("Has seleccionado la opción 1");
+              Console.ReadKey();
+              break;
+          case "2":
+          checkwinsize();
+              isValidInput = true;
+              Console.WriteLine("Has seleccionado la opción 2");
+              Console.ReadKey();
+              break;
+          case "3":
+              Console.WriteLine("Saliendo del programa");
+              Console.ReadKey();
+              return;
+          default:
+              Console.CursorVisible = false;
+              Console.Write("\x1b[1m");
+              Console.ForegroundColor=ConsoleColor.DarkRed;
+              Console.WriteLine("\nOpcion no valida\n");
+              Console.ForegroundColor=ConsoleColor.White;
+              Console.WriteLine("Pulsa la tecla [Enter] para continuar...");
+              Console.Write("\x1b[0m");
+              while (Console.ReadKey().Key != ConsoleKey.Enter) {}
+              break;
+        }
       }
     }
+
     static void checkwinsize()
     {
       if (Console.WindowWidth < 45 || Console.WindowHeight < 15)
@@ -86,7 +91,7 @@ class Program
         Console.Write("\x1b[1m");
         Console.WriteLine("La ventana es demasiado chica para continuar con la correcta ejecucion de el programa!!!\n");
         Console.ForegroundColor=ConsoleColor.White;
-        Console.WriteLine("Pulsa la tecla [Enter] para salir ó ajuste el tamaño de la ventana y pulse la tecla [C] para continuar\n");
+        Console.WriteLine("Pulsa las teclas [Ctrl + C] para salir ó ajuste el tamaño de la ventana y pulse la tecla [Enter] para continuar\n");
         Console.Write("\x1b[0m");
         while(true)
         {
@@ -100,15 +105,10 @@ class Program
                 Console.Write("\x1b[1m");
                 Console.WriteLine("El tamaño de la ventana sigue sin ser sufiente para seguir con la ejecucion de el programa");
                 Console.ForegroundColor=ConsoleColor.White;
-                Console.WriteLine("\nPulsa la tecla [Esc] para salir ó ajuste el tamaño de la ventana y pulse la tecla [Enter] para continuar\n");
+                Console.WriteLine("\nPulsa las teclas [Ctrl + C] para salir ó ajuste el tamaño de la ventana y pulse la tecla [Enter] para continuar\n");
                 Console.Write("\x1b[0m");
               }
               else{break;}
-          }
-          else if (keyInfo.Key == ConsoleKey.Escape)
-          {
-              Console.Clear();
-              Environment.Exit(0);
           }
           else{}
         }
